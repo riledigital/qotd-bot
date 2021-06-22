7cd92a95a64ca0eca662a27f41171347c879a347
Ri <ri.le@upstatement.com>
2021-06-22 19:51:50 -0400

    remove extra print

--- start.js
+++ start.js
@@ -1,29 +1,30 @@
-const config = require("./config.js");
-const SimpleQotd = require("./SimpleQotd.js");
+const config = require('./config.js');
+const SimpleQotd = require('./SimpleQotd.js');
 
 const client = new SimpleQotd(config);
 
-client.once("ready", () => {
+client.once('ready', () => {
   switch (process.env.NODE_ENV) {
-    case "dev": {
-      client.scheduleCronDaily();
+    case 'development': {
       // Ping the channel
       client
-      .getQotdChannel()
+        .getQotdChannel()
         .send(
           `QOTD-Bot deployed and ready to go! Bot version: ${process.env.npm_package_version}`
         );
+      break;
     }
-    default: {
+    case 'production': {
       // Silent start. don't ping the channel
       console.log(
         `QOTD-Bot deployed and ready to go! Bot version: ${process.env.npm_package_version}`
       );
+      break;
     }
   }
 
   client.scheduleCronDaily();
-  client.on("message", (msg) => {
+  client.on('message', (msg) => {
     if (msg.channel.id === config.QOTD_CHANNEL_ID) {
       client.handleCommand(msg, msg.content);
     }
