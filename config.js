6a0cabeae5894462e70fc7ba3c6b4c67127356c2
Ri <ri.le@upstatement.com>
2021-06-22 20:47:23 -0400

    remove static class bug

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-const envs = require("dotenv").config();
+const envs = require('dotenv').config();
 
 // CommonJS
-config = {
+const config = {
   DISCORD_TOKEN: process.env.DISCORD_TOKEN,
   AIRTABLE_API_KEY: process.env.AT_KEY,
   AIRTABLE_BASE: process.env.AT_BASE,
 
   // Q_FREQUENCY: "* * * * * *",
   // Note that this is UTC/GMT time
-  Q_FREQUENCY: "0 16 * * *",
+  Q_FREQUENCY: '0 16 * * *',
   QOTD_CHANNEL_ID: process.env.QOTD_CHANNEL_ID,
-  BOT_DISPLAY_NAME: "QOTD-Bot",
-  QUESTION_FORM_LINK: process.env.QUESTION_FORM_LINK,
+  BOT_DISPLAY_NAME: 'QOTD-Bot',
+  QUESTION_FORM_LINK: process.env.QUESTION_FORM_LINK
 };
 
 module.exports = config;
